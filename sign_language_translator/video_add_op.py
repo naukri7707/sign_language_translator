a0b25222cc60a5c245f4952f6d2ebdbbd01feffc
Naukri <naukri7707@gmail.com>
2023-12-09 18:12:05 +0800

    功能 : 新增 video to spritesheet

---
+++ sign_language_translator/video_add_op.py
@@ -0,0 +1,38 @@
+﻿import numpy as np
+import cv2
+
+
+def convert(video_path):
+    cap = cv2.VideoCapture(video_path)
+
+    success, frame1 = cap.read()
+    prvs = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+    hsv = np.zeros_like(frame1)
+    hsv[...,1] = 255
+
+    while cap.isOpened():
+        success, frame2 = cap.read()
+
+        if not success:
+            break
+
+        next = cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY)
+        flow = cv2.calcOpticalFlowFarneback(prvs,next, flow=None, 
+                pyr_scale=0.5, levels=1, winsize=15, iterations=2, poly_n=5, poly_sigma=1.1, flags=0)
+        mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
+        hsv[...,0] = ang*180/np.pi/2
+        hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
+        bgr = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+        cv2.imshow('frame2',bgr)
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
+        elif k == ord('s'):
+            cv2.imwrite('opticalfb.png',frame2)
+            cv2.imwrite('opticalhsv.png',bgr)
+        prvs = next
+
+    cv2.destroyAllWindows()
+    cap.release()
+
+convert('../~data/test/1-converted.mp4')
---
+++ sign_language_translator/video_to_sprite_sheet.py
@@ -0,0 +1,39 @@
+"""
+將影片轉換成序列圖
+"""
+import os
+import cv2
+
+def convert(input_file_path, output_file_path):
+    """
+    將影片轉換成序列圖
+    """
+    cap = cv2.VideoCapture(input_file_path)
+    
+    frame = 0
+
+    dir_name = os.path.dirname(output_file_path)
+    name_without_extension = os.path.splitext(os.path.basename(output_file_path))[0]
+
+    while cap.isOpened():
+        success, img = cap.read()
+
+        if not success:
+            break
+        
+
+        # output file path + 序列號
+        frame_name = os.path.join(dir_name, f"{name_without_extension}_f{frame:03d}.jpg")
+
+
+        # 匯出成 jpg
+        cv2.imwrite(frame_name, img)
+        
+        frame += 1
+
+    cap.release()
+
+convert(
+    "../~data/test/1.mp4",
+    "../~data/test-ok/1.mp4",
+)
